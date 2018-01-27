8b94256b6238237a65be8215e50053118e7bffa0
JJ Garzella <jjgarzella@gmail.com>
2018-01-26 20:44:58 -0700

    Add support for parsing of multiple statements.

--- src/lib.rs
+++ src/lib.rs
@@ -9,20 +9,6 @@ mod genericsyntax;
 mod parse;
 mod expr;
 
-pub fn main() {
-    // get input and interp!
-    use std::io::{self, BufRead};
-    loop {
-        print!("interpy>> ");
-        let mut input = String::new();
-        let stdin = io::stdin();
-        stdin.lock().read_line(&mut input).expect("Could not read from standard input");
-        println!("{}", interp(input))
-    }
-}
-
-
-
 pub fn interp(code: String) -> expr::Value {
   use tokenizer::Tokenizer;
   println!("Tokenizing...");
@@ -30,8 +16,11 @@ pub fn interp(code: String) -> expr::Value {
   println!("Generating Syntax...");
   let generic_syntax = genericsyntax::GenericSyntaxTree::from_tokens(&tokens);
   println!("Parsing...");
-  let expr = parse::parse_lambda(&generic_syntax);
+  let exprs = parse::parse_statements(&generic_syntax);
   println!("Interpreting...");
-  let val = expr::interp(expr, &expr::Environment::new());
-  val
+  let mut vals: Vec<expr::Value> = vec![];
+  for expr in exprs {
+    vals.push(expr::interp(expr, &expr::Environment::new()));
+  }
+  vals.pop().unwrap()
 }
--- src/parse.rs
+++ src/parse.rs
@@ -1,28 +1,28 @@
 
 use super::genericsyntax::GenericSyntaxTree;
+use super::genericsyntax::GenericSyntaxTree::*;
 use super::expr::Expr;
 
 
 
-pub fn parse_lambda(tree: &GenericSyntaxTree) -> Expr {
-    use self::GenericSyntaxTree::*;
+pub fn parse(tree: &GenericSyntaxTree) -> Expr {
     match *tree {
         Symbol(ref s) if s.parse::<isize>().is_ok() => Expr::Num(s.parse().unwrap()),
         Symbol(ref s) => Expr::Id(s.clone()),
         List(ref syntax) 
             if syntax.len() == 1 => panic!("unknown length 1 operator"),
         List(ref syntax) 
-            if syntax.len() == 2 => Expr::Apply(box parse_lambda(&syntax[0]), 
-                                                   box parse_lambda(&syntax[1])),
+            if syntax.len() == 2 => Expr::Apply(box parse(&syntax[0]), 
+                                                   box parse(&syntax[1])),
         List(ref syntax)
             if syntax.len() == 3 => match syntax[0] {
-                Symbol(ref p) if p == "+" => Expr::Plus(box parse_lambda(&syntax[1]), 
-                                                      box parse_lambda(&syntax[2])),
-                Symbol(ref t) if t == "*" => Expr::Times(box parse_lambda(&syntax[1]), 
-                                                       box parse_lambda(&syntax[2])),
+                Symbol(ref p) if p == "+" => Expr::Plus(box parse(&syntax[1]), 
+                                                      box parse(&syntax[2])),
+                Symbol(ref t) if t == "*" => Expr::Times(box parse(&syntax[1]), 
+                                                       box parse(&syntax[2])),
                 Symbol(ref l) if l == "lambda" => match syntax[1] {
                     List(ref vec) if vec.len() == 1 => match vec[1] {
-                        Symbol(ref s) => Expr::Func(s.clone(), box parse_lambda(&syntax[2])),
+                        Symbol(ref s) => Expr::Func(s.clone(), box parse(&syntax[2])),
                         _ => panic!("unsupported inside of second part of function"),
                     },
                     _ => panic!("unsupported second part of function"),
@@ -33,6 +33,18 @@ pub fn parse_lambda(tree: &GenericSyntaxTree) -> Expr {
     }
 }
 
+pub fn parse_statements(tree: &GenericSyntaxTree) -> Vec<Expr> {
+    match *tree {
+        Symbol(_) => panic!("Symbol is not a list of statements"),
+        List(ref statements) => {
+            let mut exprs: Vec<Expr> = vec![];
+            for statement in statements {
+                exprs.push(parse(statement));
+            }
+            exprs
+        }
+    }
+}
 
 //#[cfg(test)]
 //mod tests {
